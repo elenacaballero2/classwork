cdd11209d9e01977d5f203e714299a5800e26915
Elena Caballero <elena.caballero79@gmail.com>
2020-04-13 22:21:33 -0400

    Hello World in NetBeans

---
+++ Java_Classwork/HelloWorld/src/main/java/com/ec/helloworld/Hello.java
@@ -0,0 +1,19 @@
+
+/**
+ *
+ * @author elenacaballero
+ * @4/13/20
+ * @purpose: print hello world
+ */
+package com.ec.helloworld;
+
+public class Hello {
+    //this statement tells Java where the program starts
+    public static void main(String[] args){
+        //out.println tells Java to print out the text inside ()
+       System.out.println("Hello,World!"); 
+       System.out.println("My name is Elena!");
+    }
+    
+    
+}
